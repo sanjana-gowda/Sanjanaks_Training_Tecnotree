a236c47b2dfd9b6e29fe19d9557ec9541d888104
sanjana-gowda <129833501+sanjana-gowda@users.noreply.github.com>
2023-04-16 11:54:18 +0530

    Add files via upload

---
+++ reactjs/a3_app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CountdownTimer from './p3';
+
+function App() {
+  return (
+    <div>
+      <CountdownTimer initialSeconds={60} />
+    </div>
+  );
+}
+
+export default App;
