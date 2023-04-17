b697ac58e018858f5155825a7decee139ee19516
sanjana-gowda <129833501+sanjana-gowda@users.noreply.github.com>
2023-04-17 20:17:01 +0530

    Delete sql/MediaLibrary/Routes directory

--- sql/MediaLibrary/Routes/statistics.js
+++
@@ -1,9 +0,0 @@
-const books = require("../Api/Content/books.json");
-const music = require("../Api/Content/music.json");
-const videos = require("../Api/Content/videos.json");
-
-const statisticsRoute = (req, res)=>{
-    res.status(200).json({books: books.length, music: music.length, videos: videos.length});
-}
-
-module.exports = statisticsRoute;
